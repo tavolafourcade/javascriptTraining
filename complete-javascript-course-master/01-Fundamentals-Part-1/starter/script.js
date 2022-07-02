b16ae1e6f5c8a269792d6443557fae7bc634924f
tavolafourcade <tavolafourcade@gmail.com>
2022-07-01 19:56:01 -0500

    [ ADD ] Basic implementation of the challenge

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -12,3 +12,14 @@ TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1
 
 GOOD LUCK 😀
 */
+
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 92;
+const johnHeight = 1.95;
+
+const markBMI = markMass / markHeight ** 2
+const johnBMI = johnMass / johnHeight ** 2
+
+const markHigherBMI = markBMI > johnBMI
+console.log(markHigherBMI)
