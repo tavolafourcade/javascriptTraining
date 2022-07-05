a18106aafaa454d4d74b2b46a05a845d70ebef6b
tavolafourcade <tavolafourcade@gmail.com>
2022-07-05 18:55:57 -0500

    [ ADD ] Additional implementation to OBject Methods challenge

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -183,7 +183,7 @@ const person = {
 		return this.age
 	},
 	getSummary: function(){
-		return `${this.firstName} is a ${this.calcAge()}-year old ${this.job} and he has a driver's licence`
+		return `${this.firstName} is a ${this.calcAge()}-year old ${this.job} and he has ${this.hasDriversLicense ? "a" : "no"} driver's licence`
 	}
 }
 
