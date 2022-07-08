b59b43644c9b447c1e79b362e2fa8d8b7f698a0a
tavolafourcade <tavolafourcade@gmail.com>
2022-07-07 23:27:52 -0500

    [ ADD ] Section3. Challenge 4 solved Bonus implementing function with a for loop to calculate the avg from an array

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -347,3 +347,15 @@ for (let i = 0; i < bills.length; i++) {
 }
 
 console.log(bills, tips, total)
+
+// Bonus
+const calcAverage = (arr) => {
+  let sum = 0
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+  return sum / arr.length
+}
+
+console.log(calcAverage([5,10,5]))
+console.log(calcAverage(tips))
