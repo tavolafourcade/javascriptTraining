2eecfb4420275262ddee804b7b91a4e9cbba1814
tavolafourcade <tavolafourcade@gmail.com>
2022-07-09 12:15:23 -0500

    [ ADD ] Selecting and manipulating elements

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,3 +1,12 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent)
+
+//Set content of an element
+document.querySelector('.message').textContent = 'Correct Numer! 🤟';
+
+document.querySelector('.number').textContent = 25
+document.querySelector('.score').textContent = 30
+
+document.querySelector('.guess').value = 23
+console.log(document.querySelector('.guess').value)
