8455ba4cdeaa2d2f61d3d4f5e3d1516059473960
tavolafourcade <tavolafourcade@gmail.com>
2022-07-10 12:01:04 -0500

    [ ADD ] Implementing the Game Logic

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -13,12 +13,37 @@ document.querySelector('.guess').value = 23
 console.log(document.querySelector('.guess').value)
 
 */
+
+const secretNumber = Math.trunc(Math.random()*20) + 1 // 1-20
+let score = 20
+document.querySelector('.number').textContent = secretNumber
 const checkGuess = () => {
   const guess = Number(document.querySelector('.guess').value)
   console.log(guess, typeof(guess))
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number'
+  }
+  else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number! 🤟';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high! 🤔';
+    score--
+    document.querySelector('.score').textContent = score
+    } else {
+      document.querySelector('.message').textContent = 'You lost! 😭';
+      document.querySelector('.score').textContent = 0
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+    document.querySelector('.message').textContent = 'Too low! 🤔';
+    score--
+    document.querySelector('.score').textContent = score
+    } else {
+      document.querySelector('.message').textContent = 'You lost! 😭';
+      document.querySelector('.score').textContent = 0
+    }
   }
 }
 document.querySelector('.check').addEventListener('click', checkGuess);
