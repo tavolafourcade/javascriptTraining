f54edb7c9a9da7a7b0f5f89500455509451a6dbd
tavolafourcade <tavolafourcade@gmail.com>
2022-07-09 19:46:49 -0500

    [ ADD ] Handling click events

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent)
 
 //Set content of an element
@@ -10,3 +11,14 @@ document.querySelector('.score').textContent = 30
 
 document.querySelector('.guess').value = 23
 console.log(document.querySelector('.guess').value)
+
+*/
+const checkGuess = () => {
+  const guess = Number(document.querySelector('.guess').value)
+  console.log(guess, typeof(guess))
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number'
+  }
+}
+document.querySelector('.check').addEventListener('click', checkGuess);
