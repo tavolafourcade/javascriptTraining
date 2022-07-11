58b3e2da17fb7d4a55f68b73bb7f6a3578bb430a
tavolafourcade <tavolafourcade@gmail.com>
2022-07-10 22:12:15 -0500

    [ ADD ] Implementation of play again (Reset score, functionalities and styles to initial value)

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -14,9 +14,11 @@ console.log(document.querySelector('.guess').value)
 
 */
 
-const secretNumber = Math.trunc(Math.random()*20) + 1 // 1-20
+const getRandomNumber = () => {
+  return Math.trunc(Math.random()*20) + 1
+} // 1-20
+let secretNumber = getRandomNumber()
 let score = 20
-document.querySelector('.number').textContent = secretNumber
 const checkGuess = () => {
   const guess = Number(document.querySelector('.guess').value)
   console.log(guess, typeof(guess))
@@ -28,6 +30,7 @@ const checkGuess = () => {
   // When player wins
   else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number! 🤟';
+    document.querySelector('.number').textContent = secretNumber
     document.querySelector('body').style.backgroundColor = '#60b347'
     document.querySelector('.number').style.width = '30rem'
   // When guess is too high
@@ -52,4 +55,33 @@ const checkGuess = () => {
     }
   }
 }
+
+const resetGame = () => {
+  score = 20
+  secretNumber = getRandomNumber()
+  document.querySelector('body').style.backgroundColor = '#222'
+  document.querySelector('.number').style.width = '15rem'
+  document.querySelector('.number').textContent = '?'
+  document.querySelector('.message').textContent = 'Start guessing...'
+  document.querySelector('.score').textContent = score
+  document.querySelector('.guess').value=''
+
+}
 document.querySelector('.check').addEventListener('click', checkGuess);
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Implement a game reset functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
+
+document.querySelector('.again').addEventListener('click', resetGame)
