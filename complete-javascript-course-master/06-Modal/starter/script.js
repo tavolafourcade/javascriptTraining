525463ea4efc1a905cc0a17ee323caff81af41bc
tavolafourcade <tavolafourcade@gmail.com>
2022-07-11 17:36:27 -0500

    [ ADD ] Working with classes to hide and sow both the modal and the overlay

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,9 +1,23 @@
 'use strict';
 
+const openModal = () => {
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+const closeModal = () => {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+
+}
+
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
