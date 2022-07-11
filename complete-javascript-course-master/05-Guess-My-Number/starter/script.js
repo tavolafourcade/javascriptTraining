d3c3dabb5b3aee2edcf7ff3202d5561f56d52305
tavolafourcade <tavolafourcade@gmail.com>
2022-07-10 21:36:03 -0500

    [ ADD ] Manipulating background color and number width CSS styles

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -21,11 +21,16 @@ const checkGuess = () => {
   const guess = Number(document.querySelector('.guess').value)
   console.log(guess, typeof(guess))
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number'
   }
+  // When player wins
   else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number! 🤟';
+    document.querySelector('body').style.backgroundColor = '#60b347'
+    document.querySelector('.number').style.width = '30rem'
+  // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high! 🤔';
@@ -35,6 +40,7 @@ const checkGuess = () => {
       document.querySelector('.message').textContent = 'You lost! 😭';
       document.querySelector('.score').textContent = 0
     }
+  // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
     document.querySelector('.message').textContent = 'Too low! 🤔';
