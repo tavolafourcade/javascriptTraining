a74d0983c81538e18d8c1cbbf8db266621293797
tavolafourcade <tavolafourcade@gmail.com>
2022-07-15 22:47:12 -0500

    [ ADD ] Resetting the game to the initial value

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -13,15 +13,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+
+
+// Starting conditions
+const init = () => {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active'); // If the class doesn't exist will not remove anything
+}
+init()
 
 // Rolling dice functionality
 
@@ -60,7 +73,7 @@ const btnHoldAction = () =>{
     scores[activePlayer] += currentScore // When player is 1 it will add to player 1's score
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
     // 2. Check if player's score s > 100
-    if (scores[activePlayer]>= 20) {
+    if (scores[activePlayer]>= 100) {
       // Finish the game
       playing = false
       document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
@@ -74,3 +87,4 @@ const btnHoldAction = () =>{
 
 btnRoll.addEventListener('click', btnRollAction)
 btnHold.addEventListener('click', btnHoldAction)
+btnNew.addEventListener('click', init)
