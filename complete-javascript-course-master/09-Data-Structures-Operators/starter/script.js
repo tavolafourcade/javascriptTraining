08b2510d53049f275bf6680ff4c559b7ef0b219b
tavolafourcade <tavolafourcade@gmail.com>
2022-08-31 22:11:44 -0500

    [ ADD ] Destructuring arrays examples

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,69 @@ const restaurant = {
     },
   },
 };
+
+///////////////////////////////////////
+// First exercise Destructuring
+
+// Without destructuring:
+// const arr = [2,3,4]
+// const a = arr[0]
+// const b = arr[1]
+// const c = arr[2]
+
+// With destructuring:
+// const [x,y,z] = arr
+// console.log('x',x) // 2
+// console.log('y',y) // 3
+// console.log('z',z) // 4
+
+// Second exercise Destructuring
+// const [ first, second ] = restaurant.starterMenu
+// console.log('first',first) // 'Focaccia'
+// console.log('second',second) // 'Bruschetta'
+
+// const [ first, ,second ] = restaurant.starterMenu
+// console.log('first',first) // 'Focaccia'
+// console.log('second',second) // 'Garlic Bread'
+
+// Third exercise Destructuring
+// Let's say we want to switch of the main menu and the secondary menu
+// let [ main, ,secondary ] = restaurant.starterMenu;
+
+// Without destructuring:
+// const temp = main
+// main = secondary
+// secondary = temp
+// console.log(main, secondary) // 'Garlic Bread' 'Focaccia'
+
+// With destructuring:
+// [ main, secondary ] = [secondary, main]
+// console.log(main, secondary) // 'Focaccia' 'Garlic Bread'
+
+// Fourth exercise Destructuring
+// console.log(restaurant.order(2,0)) // ['Garlic Bread', 'Pizza']
+
+// Receive 2 return values from a function
+// const [ starter, main ] = restaurant.order(2,0)
+// console.log(starter, main) // 'Garlic Bread' 'Pizza'
+
+// Fifth exercise
+// What happens if we have a nested array?
+
+// const nested = [2,4,[5,6]]
+
+// // const [i,,j] = nested
+// // console.log(i,j) // 2, [5, 6]
+
+// const [i,,[j,k]] = nested
+// console.log(i,j,k) // 2, 5, 6
+
+// Sixth exercise
+// We can also set default values for the variables when we are extracting them. This is useful in the case when we don’t know the length of the array.
+
+// const [p, q, r] = [8, 9]
+// console.log(p, q, r) // 8 9 undefined
+
+// So we can set default values
+const [p = 1, q = 1, r = 1] = [8, 9]
+console.log(p, q, r) // 8 9 1
