7985ab342bef8e8f8587770625832f6a96d8fdff
tavolafourcade <tavolafourcade@gmail.com>
2022-09-04 13:59:19 -0500

    [ ADD ] Rest Pattern and Parameters

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -41,6 +41,11 @@ const restaurant = {
 
   orderPasta: function(ing1, ing2, ing3){
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`)
+  },
+
+  orderPizza: function(mainIngredient, ...otherIngredients){
+    console.log(mainIngredient)
+    console.log(otherIngredients)
   }
 };
 
@@ -245,3 +250,36 @@ const restaurant = {
 // console.log('restaurant2',restaurant.name) // La Arequipeña
 
 ///////////////////////////////////////
+// Rest Pattern and Parameters
+
+// 1. Destructuring
+
+// Spread operator, because on the right side of =
+// const arr = [1,2,...[3,4]] // [1, 2, 3, 4]
+
+// Rest operator, because on the left side of =
+// const [a, b, ...others] = [1,2,3,4,5] // a = 1, b = 2, others = [3,4,5]
+
+// const [pizza,, rissoto, ...others] = [...restaurant.mainMenu, ...restaurant.starterMenu]
+// console.log(pizza, rissoto, others) // Pizza Risotto (4) ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"]
+
+// Rest Pattern with Objects
+// const {sat, ...weekdays} = restaurant.openingHours
+// console.log(sat, weekdays) // {open: 11, close: 23} {thu: {…}, fri: {…}}
+
+// 2. Functions
+// const add = function(...numbers) {
+//   let sum = 0
+//   for (let i = 0; i < numbers.length; i++) sum += numbers[i]
+//   console.log(sum)
+// }
+
+// add(2,3) // 5
+// add(5,3,7,2) // 17
+// add(8,2,5,3,2,1,4) // 25
+
+// const x = [23,5,7]
+// add(...x) // 35
+
+// restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach') // mushrooms (3) ['onion', 'olives', 'spinach']
+// restaurant.orderPizza('mushrooms') // mushrooms (0) []
