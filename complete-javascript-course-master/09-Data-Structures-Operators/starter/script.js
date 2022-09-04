112e5088dd791613fac005eef8b270710ef29234
tavolafourcade <tavolafourcade@gmail.com>
2022-09-04 18:47:36 -0500

    [ ADD ] The Nullish Coalescing Operator ??

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -323,3 +323,14 @@ const restaurant = {
 // console.log('hello' && 5 && 'AND') // AND
 
 ///////////////////////////////////////
+// The Nullish Coalescing Operator (??)
+
+// restaurant.numGuests = 0
+// const guests = restaurant.numGuests || 10
+// console.log(guests) // 10
+
+// // Nullish: null and undefined (NOT 0 or '')
+// const guestCorrect = restaurant.numGuests ?? 10
+// console.log(guestCorrect) // 0
+
+///////////////////////////////////////
