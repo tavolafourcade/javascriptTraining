7d45688fd0419d3af25f76c5c4dad9d63d391548
tavolafourcade <tavolafourcade@gmail.com>
2022-09-06 08:33:54 -0500

    [ ADD ] Implementation of the for-of loop

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -397,78 +397,96 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 GOOD LUCK 😀
 */
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
-// 1. Create one player array for each team (variables 'players1' and 'players2')
-const [ players1, players2 ] = game.players
-console.log(players1, players2)
+// // 1. Create one player array for each team (variables 'players1' and 'players2')
+// const [ players1, players2 ] = game.players
+// console.log(players1, players2)
 
-// 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
-// const gk = game.players[0][0]
-// const fieldPlayers = [, ...game.players[0].slice(1)] // [ 'Pavard', 'Martinez', 'Alaba', 'Davies', 'Kimmich', 'Goretzka', 'Coman', 'Muller', 'Gnarby', 'Lewandowski' ]
-const [gk, ...fieldPlayers] = players1
-console.log(gk, fieldPlayers)
+// // 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+// // const gk = game.players[0][0]
+// // const fieldPlayers = [, ...game.players[0].slice(1)] // [ 'Pavard', 'Martinez', 'Alaba', 'Davies', 'Kimmich', 'Goretzka', 'Coman', 'Muller', 'Gnarby', 'Lewandowski' ]
+// const [gk, ...fieldPlayers] = players1
+// console.log(gk, fieldPlayers)
 
-// 3. Create an array 'allPlayers' containing all players of both teams (22 players)
-// const allPlayers = [...game.players[0],...game.players[1]] // [ 'Neuer', 'Pavard', 'Martinez', 'Alaba', 'Davies', 'Kimmich', 'Goretzka', 'Coman', 'Muller', 'Gnarby', 'Lewandowski', 'Burki', 'Schulz', 'Hummels', 'Akanji', 'Hakimi', 'Weigl', 'Witsel', 'Hazard', 'Brandt', 'Sancho', 'Gotze' ]
-const allPlayers = [...players1, ...players2]
-console.log(allPlayers)
+// // 3. Create an array 'allPlayers' containing all players of both teams (22 players)
+// // const allPlayers = [...game.players[0],...game.players[1]] // [ 'Neuer', 'Pavard', 'Martinez', 'Alaba', 'Davies', 'Kimmich', 'Goretzka', 'Coman', 'Muller', 'Gnarby', 'Lewandowski', 'Burki', 'Schulz', 'Hummels', 'Akanji', 'Hakimi', 'Weigl', 'Witsel', 'Hazard', 'Brandt', 'Sancho', 'Gotze' ]
+// const allPlayers = [...players1, ...players2]
+// console.log(allPlayers)
 
-// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'] // [ 'Neuer', 'Pavard', 'Martinez', 'Alaba', 'Davies', 'Kimmich', 'Goretzka', 'Coman', 'Muller', 'Gnarby', 'Lewandowski', 'Thiago', 'Coutinho', 'Perisic' ]
+// // 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'] // [ 'Neuer', 'Pavard', 'Martinez', 'Alaba', 'Davies', 'Kimmich', 'Goretzka', 'Coman', 'Muller', 'Gnarby', 'Lewandowski', 'Thiago', 'Coutinho', 'Perisic' ]
 
-// 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
-// const { team1, x: draw, team2 } = game.odds // 1.33 3.25 6.5
-const {odds:{team1, x: draw, team2}} = game
-console.log(team1) // 1.33
-console.log(draw) // 3.25
+// // 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+// // const { team1, x: draw, team2 } = game.odds // 1.33 3.25 6.5
+// const {odds:{team1, x: draw, team2}} = game
+// console.log(team1) // 1.33
+// console.log(draw) // 3.25
 
-// 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
-const printGoals = function(...names){
-  console.log(...names)
-  console.log(`${names.length} goals were scored`)
+// // 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+// const printGoals = function(...names){
+//   console.log(...names)
+//   console.log(`${names.length} goals were scored`)
+// }
+// printGoals(...game.scored) // Octavio Laura
+
+// // 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+// team1 < team2 && console.log('Team 1 is more likely to win')
+// team1 > team2 && console.log('Team 2 is more likely to win')
+
+///////////////////////////////////////
+// The for-of Loop
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu]
+
+for (const item of menu) console.log(item)
+
+// Getting the index of the item
+for (const item of menu.entries()) {
+  console.log(`${item[0] + 1}: ${item[1]}`)
 }
-printGoals(...game.scored) // Octavio Laura
 
-// 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
-team1 < team2 && console.log('Team 1 is more likely to win')
-team1 > team2 && console.log('Team 2 is more likely to win')
+for (const [i, item] of menu.entries()) {
+  console.log(`${i + 1}: ${item}`)
+}
+
+console.log([...menu.entries()])
