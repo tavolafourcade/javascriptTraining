fcdcd6b5fac7fe5b74808e307e7860e2bee7d301
tavolafourcade <tavolafourcade@gmail.com>
2022-09-08 22:03:03 -0500

    [ ADD ] Looping Objects: Objects Keys, Values and Entries

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -566,3 +566,34 @@ GOOD LUCK 😀
 // else console.log('User array empty')
 
 ///////////////////////////////////////
+
+// Looping Objects: Object Keys, Values and Entries
+
+// for (const day of Object.keys(restaurant.openingHours)){
+//   console.log(day)
+// }
+
+// Property names
+// const properties = Object.keys(restaurant.openingHours)
+// let openStr = `We are open on ${properties.length} days: `
+
+
+// for (const day of properties){
+//   openStr += `${day}, `
+// }
+
+// console.log(openStr)
+
+// Property values
+
+// const values = Object.values(restaurant.openingHours)
+// console.log(values)
+
+// Entries object
+
+// const entries = Object.entries(restaurant.openingHours)
+// // console.log(entries)
+
+// for (const [key,{open, close}] of entries){
+//   console.log(`On ${key} we open at ${open} and close at ${close}`)
+// }
