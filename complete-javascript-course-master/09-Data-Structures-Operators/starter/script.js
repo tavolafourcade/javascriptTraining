bc8f836187b297e65771f8c267891dee1c80f913
tavolafourcade <tavolafourcade@gmail.com>
2022-09-14 09:13:04 -0500

    [ UPDATE ] Commented previous code

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -880,39 +880,40 @@ Let's continue with our football betting app! This time, we have a map with a lo
 GOOD LUCK 😀
 */
 
-const gameEvents = new Map([
-  [17, '⚽️ GOAL'],
-  [36, '🔁 Substitution'],
-  [47, '⚽️ GOAL'],
-  [61, '🔁 Substitution'],
-  [64, '🔶 Yellow card'],
-  [69, '🔴 Red card'],
-  [70, '🔁 Substitution'],
-  [72, '🔁 Substitution'],
-  [76, '⚽️ GOAL'],
-  [80, '⚽️ GOAL'],
-  [92, '🔶 Yellow card'],
-]);
+// const gameEvents = new Map([
+//   [17, '⚽️ GOAL'],
+//   [36, '🔁 Substitution'],
+//   [47, '⚽️ GOAL'],
+//   [61, '🔁 Substitution'],
+//   [64, '🔶 Yellow card'],
+//   [69, '🔴 Red card'],
+//   [70, '🔁 Substitution'],
+//   [72, '🔁 Substitution'],
+//   [76, '⚽️ GOAL'],
+//   [80, '⚽️ GOAL'],
+//   [92, '🔶 Yellow card'],
+// ]);
 
-// 1. Create an array 'events' of the different game events that happened (no duplicates)
-const events = [...new Set(gameEvents.values())]
-console.log('EVENTS',events)
+// // 1. Create an array 'events' of the different game events that happened (no duplicates)
+// const events = [...new Set(gameEvents.values())]
+// console.log('EVENTS',events)
 
-// 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
-gameEvents.delete(64)
-console.log('gameEvents',gameEvents)
+// // 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+// gameEvents.delete(64)
+// console.log('gameEvents',gameEvents)
 
-// 3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
-console.log(gameEvents.size)
-const time = [...gameEvents.keys()].pop()
-const eventsAvg = time / gameEvents.size
-console.log(`An event happened, on average, every ${eventsAvg} minutes`)
+// // 3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+// console.log(gameEvents.size)
+// const time = [...gameEvents.keys()].pop()
+// const eventsAvg = time / gameEvents.size
+// console.log(`An event happened, on average, every ${eventsAvg} minutes`)
 
-// 4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
-//      [FIRST HALF] 17: ⚽️ GOAL
+// // 4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+// //      [FIRST HALF] 17: ⚽️ GOAL
 
+// for (const [key, value] of gameEvents) {
+//   const term = key < 45 ? '[FIRST HALF]' : '[SECOND HALF]'
+//   console.log(`${term} ${key}: ${value}`)
+// }
 
-for (const [key, value] of gameEvents) {
-  const term = key < 45 ? '[FIRST HALF]' : '[SECOND HALF]'
-  console.log(`${term} ${key}: ${value}`)
-}
+///////////////////////////////////////
