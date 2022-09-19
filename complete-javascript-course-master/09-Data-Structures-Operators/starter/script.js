60c30ef0840e2357ae1526efa13d2ea55a6b5ed9
tavolafourcade <tavolafourcade@gmail.com>
2022-09-18 20:36:47 -0500

    [ ADD ] Data Structures, Modern Operators and Strings Challenge 4: Own implementation

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1110,3 +1110,57 @@ GOOD LUCK 😀
 // planesInLine(5) // There are 5 planes in line ✈✈✈✈✈
 
 ///////////////////////////////////////
+
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', ()=>{
+  const text = document.querySelector('textarea').value; // get the text from the textarea
+  // 1. Split the text considering _
+  // 2. Convert the first letter of the second word to uppercase
+  // 3. Join the words
+  // 4. Log the result
+  const rows = text.split('\n')
+  const rowsTrimmed = rows.map(row => {
+    return row.trim()
+  })
+
+  rowsTrimmed.forEach((row, index) => {
+    let [first, second] =row.toLowerCase().split('_')
+    second = second.replace(second[0], second[0].toUpperCase())
+
+    const rowCamelCase = first + second
+    const output = rowCamelCase.padEnd(20, ' ') + '✅'.repeat((index + 1))
+    console.log(output)
+  })
+})
