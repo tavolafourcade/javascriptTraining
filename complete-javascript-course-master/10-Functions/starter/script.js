da4d63f63cbcf8414931df4ecde10fe05ff3c2e4
tavolafourcade <tavolafourcade@gmail.com>
2022-09-21 21:03:50 -0500

    [ ADD ] Default parameters implementation

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,1 +1,52 @@
 'use strict';
+
+
+// 1. Default Parameters
+
+// const bookings = []
+
+// const createBooking = function(flightNum, numPassenger, price){
+//   const booking = {
+//     flightNum,
+//     numPassenger,
+//     price
+//   }
+
+//   console.log(booking)
+//   bookings.push(booking)
+// }
+
+// createBooking('LH123')
+
+// Old way of setting default parameters (ES05)
+
+
+// const createBooking = function(flightNum, numPassenger, price){
+//   numPassenger = numPassenger || 1
+//   price = price || 199
+//   const booking = {
+//     flightNum,
+//     numPassenger,
+//     price
+//   }
+
+//   console.log(booking)
+//   bookings.push(booking)
+// }
+
+// createBooking('LH123')
+
+// New way of setting default parameters (ES06)
+// const createBooking = function(flightNum, numPassenger = 1, price = 199 * numPassenger){
+
+//   const booking = {
+//     flightNum,
+//     numPassenger,
+//     price
+//   }
+
+//   console.log(booking)
+//   bookings.push(booking)
+// }
+
+// createBooking('LH123', 2)
