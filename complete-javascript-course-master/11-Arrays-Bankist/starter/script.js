fa4d6a0cfe57617870373c1da5717618d7f14654
tavolafourcade <tavolafourcade@gmail.com>
2022-09-30 15:13:14 -0500

    [ ADD ] forEach with maps and sets

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -65,11 +65,11 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -152,3 +152,26 @@ const currencies = new Map([
 // })
 
 ///////////////////////////////////////
+
+// 4. forEach with Maps and Sets
+
+// Map
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+console.log('currencies',currencies) // Map(3) {"USD" => "United States dollar", "EUR" => "Euro", "GBP" => "Pound sterling"}
+
+currencies.forEach((value, key, map)=>{
+  console.log(`${key}: ${value}`)
+})
+
+// Set
+console.log('---- SET ----');
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log('currenciesUnique Set',currenciesUnique) // Set(3) {"USD", "GBP", "EUR"}
+
+currenciesUnique.forEach((value, _, map)=>{
+  console.log(`${value}: ${value}`)
+})
