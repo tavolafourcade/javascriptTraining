51e546ec1b26a4000769362d5fca239ab6c82a0e
tavolafourcade <tavolafourcade@gmail.com>
2022-10-02 22:08:36 -0500

    [ ADD ] Create movement rows dynamically

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -181,3 +181,24 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // 5. Project: Bankist App
 
 /////////////////////////////////////////////////
+
+// 6. Creating DOM elements
+const displayMovements = function(movements){
+
+  // Clear the movements container
+  containerMovements.innerHTML = ''
+  
+  movements.forEach(function(mov, i){
+
+    const type = mov > 0 ? 'deposit' : 'withdrawal'
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+      <div class="movements__value">${mov}</div>
+    </div>`
+
+    containerMovements.insertAdjacentHTML('afterbegin', html)
+  })
+}
+
+displayMovements(account1.movements)
