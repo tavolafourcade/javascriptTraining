e21ad43f7334a141e57040c240234ee33de70cfb
tavolafourcade <tavolafourcade@gmail.com>
2022-10-08 16:06:40 -0500

    [ UPDATE ] Commented previous section code

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -432,15 +432,17 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
-const calcAverageHumanAge = (ages) => {
-  const avgAge = ages
-  .map(age => age <= 2 ? 2 * age : 16 + age * 4)
-  .filter(dog => dog >= 18)
-  .reduce((acc, age, i, arr)=> (acc + age)/arr.length)
+// const calcAverageHumanAge = (ages) => {
+//   const avgAge = ages
+//   .map(age => age <= 2 ? 2 * age : 16 + age * 4)
+//   .filter(dog => dog >= 18)
+//   .reduce((acc, age, i, arr)=> (acc + age)/arr.length)
 
-  return avgAge
-}
+//   return avgAge
+// }
 
-const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])
-const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])
-console.log(avg1, avg2)
+// const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])
+// const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])
+// console.log(avg1, avg2)
+
+///////////////////////////////////////
