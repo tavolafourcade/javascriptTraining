289dac74cdc52943cdde3715208c37d602aa39f7
tavolafourcade <tavolafourcade@gmail.com>
2022-10-11 21:16:45 -0500

    [ UPDATE ] Commented previous section code

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -665,23 +665,24 @@ btnSort.addEventListener('click', function(e){
 // 23. More Ways of Creating and Filling Arrays
 
 // Empty arrays + fill method
-const x = new Array(7) // [empty x 7] creates an array with 7 empty elements
+// const x = new Array(7) // [empty x 7] creates an array with 7 empty elements
 
-x.fill(1) // [1, 1, 1, 1, 1, 1, 1]
+// x.fill(1) // [1, 1, 1, 1, 1, 1, 1]
 
-x.fill(1, 3, 5) // [empty x 3, 1, 1, empty x 2]
+// x.fill(1, 3, 5) // [empty x 3, 1, 1, empty x 2]
 
-const arr = [1, 2, 3, 4, 5, 6, 7]
-arr.fill(23, 4, 6) // [1, 2, 3, 4, 23, 23, 7]
+// const arr = [1, 2, 3, 4, 5, 6, 7]
+// arr.fill(23, 4, 6) // [1, 2, 3, 4, 23, 23, 7]
 
 // Array.from
-const y = Array.from({length: 7}, () => 1) // [1, 1, 1, 1, 1, 1, 1]
+// const y = Array.from({length: 7}, () => 1) // [1, 1, 1, 1, 1, 1, 1]
 
 // Create an array from 1 to 7
-const w = Array.from({length: 7}, (cur, i) => i + 1) // [1, 2, 3, 4, 5, 6, 7]
+// const w = Array.from({length: 7}, (cur, i) => i + 1) // [1, 2, 3, 4, 5, 6, 7]
 // cur: current element
 // i: index
-const z = Array.from({length: 7}, (_, i) => i + 1) // [1, 2, 3, 4, 5, 6, 7]
+
+// const z = Array.from({length: 7}, (_, i) => i + 1) // [1, 2, 3, 4, 5, 6, 7]
 // _: throw away variable
 
 // Working with the movements from the UI
