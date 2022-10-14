c8dc1cb4c8330fda2f925610017fb9590ee95b00
tavolafourcade <tavolafourcade@gmail.com>
2022-10-14 12:17:52 -0500

    [ UPDATE ]Commented previous section code

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -297,47 +297,49 @@ btnSort.addEventListener('click', function (e) {
 
 // Square root
 
-console.log(Math.sqrt(25)) // 5
-console.log(25 ** (1 / 2)) // 5
+// console.log(Math.sqrt(25)) // 5
+// console.log(25 ** (1 / 2)) // 5
 
-console.log(Math.max(5, 18, 23, 11, 2)) // 23
-console.log(Math.max(5, 18, '23', 11, 2)) // 23
-console.log(Math.max(5, 18, '23px', 11, 2)) // NaN
+// console.log(Math.max(5, 18, 23, 11, 2)) // 23
+// console.log(Math.max(5, 18, '23', 11, 2)) // 23
+// console.log(Math.max(5, 18, '23px', 11, 2)) // NaN
 
-console.log(Math.min(5, 18, 23, 11, 2)) // 2
+// console.log(Math.min(5, 18, 23, 11, 2)) // 2
 
-console.log(Math.PI) // 3.141592653589793
+// console.log(Math.PI) // 3.141592653589793
 
 // Calculate area of a circle
 
-console.log(Math.PI * Number.parseFloat('10px') ** 2) // 314.1592653589793
+// console.log(Math.PI * Number.parseFloat('10px') ** 2) // 314.1592653589793
 
 // Generate random number
 
-console.log(Math.random()) // 0.20843075451160953
+// console.log(Math.random()) // 0.20843075451160953
 
-console.log(Math.trunc(Math.random() * 6) + 1) // 1-6
+// console.log(Math.trunc(Math.random() * 6) + 1) // 1-6
 
-const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + 1) + min
-console.log(randomInt(10, 20)) // 10-20
+// const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + 1) + min
+// console.log(randomInt(10, 20)) // 10-20
 
 // Rounding integers
 
-console.log(Math.trunc(23.8)) // 23
+// console.log(Math.trunc(23.8)) // 23
 
-console.log(Math.round(23.3)) // 23
+// console.log(Math.round(23.3)) // 23
 
-console.log(Math.ceil(23.3)) // 24
+// console.log(Math.ceil(23.3)) // 24
 
-console.log(Math.floor(23.9)) // 23
-console.log(Math.floor('23.9')) // 23
+// console.log(Math.floor(23.9)) // 23
+// console.log(Math.floor('23.9')) // 23
 
-console.log(Math.trunc(-23.30)) // -23
-console.log(Math.floor(-23.30)) // -24
+// console.log(Math.trunc(-23.30)) // -23
+// console.log(Math.floor(-23.30)) // -24
 
-// Rounding decimals
+// // Rounding decimals
 
-console.log((2.7).toFixed(0)) // 3
-console.log((2.7).toFixed(3)) // 2.700
-console.log((2.345).toFixed(2)) // 2.35
-console.log(+(2.345).toFixed(2)) // 2.35 as a number
+// console.log((2.7).toFixed(0)) // 3
+// console.log((2.7).toFixed(3)) // 2.700
+// console.log((2.345).toFixed(2)) // 2.35
+// console.log(+(2.345).toFixed(2)) // 2.35 as a number
+
+///////////////////////////////////////
