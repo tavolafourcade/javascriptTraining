d1f4ce9b1e7206880da49cdbaa598686225e66fb
tavolafourcade <tavolafourcade@gmail.com>
2022-10-15 18:13:30 -0500

    [ UPDATE ] Commented previous section code

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -346,26 +346,26 @@ btnSort.addEventListener('click', function (e) {
 
 // 3. The Remainder Operator
 
-console.log(5 % 2) // 1
+// console.log(5 % 2) // 1
 
-console.log(5 / 2) // 2.5 => 5 = 2 * 2 + 1
+// console.log(5 / 2) // 2.5 => 5 = 2 * 2 + 1
 
-console.log(8 % 3) // 2
+// console.log(8 % 3) // 2
 
-console.log(8 / 3) // 2.6666666666666665 => 8 = 2 * 3 + 2
+// console.log(8 / 3) // 2.6666666666666665 => 8 = 2 * 3 + 2
 
 
-const isEven = n => n % 2 === 0
-console.log(isEven(8)) // true
-console.log(isEven(23)) // false
+// const isEven = n => n % 2 === 0
+// console.log(isEven(8)) // true
+// console.log(isEven(23)) // false
 
-// Example with our application
+// // Example with our application
 
-labelBalance.addEventListener('click', function () {
-  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
-    // 0, 2, 4, 6
-    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
-    // 0, 3, 6, 9
-    if (i % 3 === 0) row.style.backgroundColor = 'blue';
-  });
-});
+// labelBalance.addEventListener('click', function () {
+//   [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+//     // 0, 2, 4, 6
+//     if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+//     // 0, 3, 6, 9
+//     if (i % 3 === 0) row.style.backgroundColor = 'blue';
+//   });
+// });
