a30174488b80e1354156cc4f0f1eade2aab48045
tavolafourcade <tavolafourcade@gmail.com>
2022-10-25 13:19:41 -0500

    [ ADD ] 5. Implementing Smooth Scrolling: Old school way

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -127,3 +127,34 @@ logo.className = 'octavio'
 
 
 ///////////////////////////////////////
+
+// 5. Implementing Smooth Scrolling
+
+// Old school
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords)
+
+  console.log(e.target.getBoundingClientRect())
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset)
+
+  console.log('height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth)
+
+  // Scrolling
+  // window.scrollTo(s1coords.left, s1coords.top) // not smooth movement to the section 1
+  // This top is relative to the viewport, no to the document
+  // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset) // not smooth movement to the section 1
+
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth' // smooth movement to the section 1
+  })
+
+})
