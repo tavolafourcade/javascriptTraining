14320ef9a3d021038f48e0c2cfa3733f41553fbd
tavolafourcade <tavolafourcade@gmail.com>
2022-10-27 19:53:49 -0500

    [ ADD ] 8. Event propagation in practice

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,6 +32,36 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+
+///////////////////////////////////////
+
+// Page Navigation
+
+// document.querySelectorAll('.nav__link').forEach(function(e){
+//   e.addEventListener('click', function(e) {
+//     e.preventDefault()
+
+//     const id = this.getAttribute('href')
+
+//     document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+//     console.log('LINK')
+//   })
+// })
+
+// Event delegation
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function(e) {
+  e.preventDefault()
+
+  // Matching strategy
+  if(e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href')
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+  }
+})
 ///////////////////////////////////////
 
 // Selecting elements
@@ -129,11 +161,6 @@ logo.className = 'octavio'
 ///////////////////////////////////////
 
 // 5. Implementing Smooth Scrolling
-
-
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-
-const section1 = document.querySelector('#section--1');
 
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
