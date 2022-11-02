b5a8926e785ad354ccdeb3c71898c6d8797fdded
tavolafourcade <tavolafourcade@gmail.com>
2022-11-01 22:39:21 -0500

    [ ADD ] 3. Constructor Functions and the new Operator

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// 3. Constructor Functions and the new Operator
+const Person = function(firstName, birthYear){
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never create a method inside a constructor function
+  // this.calcAge = function(){
+  //   console.log(2037 - this.birthYear);
+  // }
+}
+
+const octavio = new Person('Octavio', 1991)
+console.log(octavio)
+
+console.log(octavio instanceof Person) // true
+
+///////////////////////////////////////
