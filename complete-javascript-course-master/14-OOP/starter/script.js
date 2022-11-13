1ff5a1338d924a585d9783209ebaec3474644556
tavolafourcade <tavolafourcade@gmail.com>
2022-11-13 12:40:26 -0500

    [ ADD ] Section 14: Object-Oriented Programming (OOP) with Javascript: Challenge #2

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -199,29 +199,88 @@ Person.hey = function () {
 // }
 
 // We are implementing Prototype inheritance in a different way
-const steven = Object.create(PersonProto)
-steven.name = 'Steven'
-steven.birthYear = 2002
+// const steven = Object.create(PersonProto)
+// steven.name = 'Steven'
+// steven.birthYear = 2002
 // steven.calcAge() // 35
 
-console.log(steven.__proto__ === PersonProto) // true
+// console.log(steven.__proto__ === PersonProto) // true
 
 
 // A better way of setting properties
-const sarah = Object.create(PersonProto)
+// const sarah = Object.create(PersonProto)
 
-const PersonProto = {
-  calcAge(){
-    console.log(2037 - this.birthYear);
-  },
+// const PersonProto = {
+//   calcAge(){
+//     console.log(2037 - this.birthYear);
+//   },
 
-  init(firstName, birthYear){
-    this.firstName = firstName
-    this.birthYear = birthYear
-  }
-}
+//   init(firstName, birthYear){
+//     this.firstName = firstName
+//     this.birthYear = birthYear
+//   }
+// }
 
-sarah.init('Sarah', 1979)
+// sarah.init('Sarah', 1979)
 // sarah.calcAge() // 58
 
 ///////////////////////////////////////
+
+// 12. Coding Challenge 2
+
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀
+*/
+
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+class CarCl {
+  constructor(make, speed){
+    this.make = make
+    this.speed = speed;
+  }
+
+  accelerate(){
+    this.speed += 10
+    console.log(`${this.make} is going at ${this.speed} km/h`)
+  }
+
+  brake(){
+    this.speed -= 5
+    console.log(this.speed)
+  }
+
+  get speedUS() 
+  {
+    return this.speed / 1.6
+  }
+
+  set speedUS(speed){
+    this.speed = speed * 1.6
+  }
+}
+
+const ford = new CarCl('Ford', 120)
+console.log(ford.speedUS) // 75
+ford.accelerate() // Ford is going at 130 km/h
+ford.brake()
+ford.speedUS = 50
+console.log(ford) // CarCl {make: 'Ford', speed: 80}
