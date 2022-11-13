fd7825ebc82e7cd02574a15e93b371016327625b
tavolafourcade <tavolafourcade@gmail.com>
2022-11-12 20:31:19 -0500

    [ ADD ] Object.create implementation

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -189,3 +189,37 @@ Person.hey = function () {
 // PersonCl.hey()
 
 ///////////////////////////////////////
+
+// 11. Object.create
+
+// const PersonProto = {
+//   calcAge(){
+//     console.log(2037 - this.birthYear);
+//   }
+// }
+
+// We are implementing Prototype inheritance in a different way
+const steven = Object.create(PersonProto)
+steven.name = 'Steven'
+steven.birthYear = 2002
+steven.calcAge() // 35
+
+console.log(steven.__proto__ === PersonProto) // true
+
+
+// A better way of setting properties
+const sarah = Object.create(PersonProto)
+
+const PersonProto = {
+  calcAge(){
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear){
+    this.firstName = firstName
+    this.birthYear = birthYear
+  }
+}
+
+sarah.init('Sarah', 1979)
+sarah.calcAge() // 58
