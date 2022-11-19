535cb56104eea2224ba31ee6cb1a00d7aae21dc6
tavolafourcade <tavolafourcade@gmail.com>
2022-11-19 17:29:55 -0500

    [ UPDATE ] Commented previous section code

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -456,12 +456,14 @@ class Account {
 
 }
 
-const acc1 = new Account('octavio', 'EUR', 1111)
+// const acc1 = new Account('octavio', 'EUR', 1111)
 
 // acc1.movements.push(250)
 // acc1.movements.push(-140)
-acc1.deposit(250)
-acc1.withdraw(140)
-acc1.requestLoan(1000)
-acc1.approveLoan(1000)
-console.log(acc1)
+// acc1.deposit(250)
+// acc1.withdraw(140)
+// acc1.requestLoan(1000)
+// acc1.approveLoan(1000)
+// console.log(acc1)
+
+///////////////////////////////////////
