895d880c72724d607272034e31f33f565191ecf0
tavolafourcade <tavolafourcade@gmail.com>
2022-11-23 10:56:50 -0500

    [ ADD ] Section 15: Mapty App: OOP, Geolocation, External Libraries, and More!: 2. Using the Geolocation API

--- complete-javascript-course-master/15-Mapty/starter/script.js
+++ complete-javascript-course-master/15-Mapty/starter/script.js
@@ -10,3 +10,17 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if(navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function(position){
+      // console.log('POSITION', position) // GeolocationPosition {coords: GeolocationCoordinates, timestamp: 1669154761313}
+      // console.log('Latitude', position.coords.latitude) // -12.12347
+      // console.log('Longitude', position.coords.longitude) // 40.73061
+      const { latitude } = position.coords
+      const { longitude } = position.coords
+      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`)
+
+  }, function(){
+    alert('Could not get your position')
+  })
