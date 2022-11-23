7c628816f06f4f252701bb5c6e6ef79cb8b454ea
tavolafourcade <tavolafourcade@gmail.com>
2022-11-23 12:32:52 -0500

    [ ADD ] 4. Displaying a Map Marker

--- complete-javascript-course-master/15-Mapty/starter/script.js
+++ complete-javascript-course-master/15-Mapty/starter/script.js
@@ -19,7 +19,7 @@ if(navigator.geolocation)
       // console.log('Longitude', position.coords.longitude) // 40.73061
       const { latitude } = position.coords
       const { longitude } = position.coords
-      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`)
+      // console.log(`https://www.google.pt/maps/@${latitude},${longitude}`)
 
       const coords = [latitude, longitude]
 
@@ -29,10 +29,21 @@ if(navigator.geolocation)
           attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map);
 
-      L.marker(coords).addTo(map)
-          .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-          .openPopup();
+      
 
+      map.on('click', function(mapEvent){
+        const { lat, lng } = mapEvent.latlng
+        L.marker([lat, lng]).addTo(map)
+          .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup'
+          }))
+          .setPopupContent('Workout')
+          .openPopup();
+      })
   }, function(){
     alert('Could not get your position')
   })
